4971f5918628d57340de247b1bee601b984a904b
Jack <jakk.elkes@gmail.com>
2021-05-03 20:39:55 +0100

    Updates to Py File

--- WebServer.py
+++ WebServer.py
@@ -55,5 +55,4 @@ def update_line_chart(timegrp):
     return fig
 
 #app.run_server(debug=False, host='0.0.0.0', port=8050)
-if __name__ = '__main__':
-    app.run_server(debug=True)
+app.run_server(debug=True)
