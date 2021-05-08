711c5bab99b1a8846e5cd3fb6ddae9eef9a5b5d3
Jack Elkes <jakk.elkes@gmail.com>
2021-05-08 12:21:33 +0100

    changing python specification

--- WebServer.py
+++ WebServer.py
@@ -55,4 +55,5 @@ def update_line_chart(timegrp):
     return fig
 
 #app.run_server(debug=False, host='0.0.0.0', port=8050)
-app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
