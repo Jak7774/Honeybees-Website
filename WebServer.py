9cac68ddc664201b61764f9a4e956ebd6efcfecd
Jack Elkes <Jak7774@users.noreply.github.com>
2021-07-20 16:01:30 +0100

    Update WebServer.py

--- WebServer.py
+++ WebServer.py
@@ -82,8 +82,8 @@ def update_line_chart(humid_timegrp):
 
 def update_line_chart(weight_timegrp):
     mask = weight_plt.daygrp.isin(weight_timegrp)
-    wfig = px.line(weight_plt[mask],
-                 x = 'datetime', y='reading', color='weightsensor', #trendline="lowess", 
+    wfig = px.scatter(weight_plt[mask],
+                 x = 'datetime', y='reading', color='weightsensor', trendline="lowess", 
                   labels={'datetime': "Date & Time of Reading",
                           'reading': "Sensor Value"},
                   title="Weight (Kg)")
