b4875519650b3bb5072e4cb2aae379c52b271792
Jack Elkes <Jak7774@users.noreply.github.com>
2024-05-28 18:19:51 +0100

    Update BeeServer.py

--- BeeServer.py
+++ BeeServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 import matplotlib
